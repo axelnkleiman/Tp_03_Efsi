b750be0379aea1c8e4f519c575fb6280e6469dca
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2024-04-15 14:26:39 -0300

    javascript2

--- script.js
+++ script.js
@@ -20,22 +20,44 @@ class Proyecto {
     }
 }
 
-let ListaProyectos;
-let Proyecto;
+let ListaProyectos = [];
+let proyecto;
 
 function AgregarProyecto(){
-    if (ListaProyectos.indexOf(Verificacion(document.getElementById('nombre').value)) == null) {
-        proyecto = new Proyecto(document.getElementById('nombre').value, document.getElementById('descripcion').value, null)
+    if (ListaProyectos == null || Verificacion(ListaProyectos, nombre) == false) {
+        proyecto = new Proyecto(document.getElementById('nombre').value, document.getElementById('descripcion').value, [{}])
         ListaProyectos.push(proyecto);
     }
     else {
         document.getElementById('error').innerHTML = 'Error de creacion, nombre repetido'
+        console.log('Error de creacion, nombre repetido')
     }
+    console.log(ListaProyectos)
 }
 function AgregarTareaAProyecto(){
-    tarea = new Tarea(document.getElementById('descripcion2').value, false, document.getElementById('fechaVencimiento'))
-    ListaProyectos.indexOf(Verificacion(document.getElementById('nombre').value))
+    tarea = new Tarea(document.getElementById('descripcion2').value, false, document.getElementById('fecha  '))
+    const index = Busqueda(ListaProyectos, document.getElementById('nombreProyecto').value)
+    ListaProyectos[index].pushearListaTareas(tarea)
+    console.log(tarea)
+    console.log(ListaProyectos)
 }
-function Verificacion(value, index, array, nombre){
-    return value.nombreProyecto == nombre
+function Verificacion(ListaProyectos, nombre){
+    let retorno = false;
+    for (let index = 0; index < ListaProyectos.length; index++) {
+        const element = ListaProyectos[index];
+        if (nombre == element.nombreProyecto) {
+            retorno = true;
+        }
+    }
+    return retorno;
+}
+function Busqueda(ListaProyectos, nombre){
+    let retorno;
+    for (let index = 0; index < ListaProyectos.length; index++) {
+        const element = ListaProyectos[index];
+        if (nombre == element.nombreProyecto) {
+            retorno = index;
+        }
+    }
+    return retorno;
 }
